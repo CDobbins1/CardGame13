99b060743b1f68baa8dda113288519c4a871c65f
CDobbins1 <cdobsc01@hotmail.com>
2021-08-19 10:29:57 -0700

    Added generic RelayCommand for methods with parameters.

--- src/CardGame13.GUI/Commands/RelayCommand.cs
+++ src/CardGame13.GUI/Commands/RelayCommand.cs
@@ -14,4 +14,16 @@ namespace CardGame13.GUI.Commands
 
         public override void Execute(object parameter) => ExecuteDelegate.Invoke();
     }
+
+    public class RelayCommand<T> : BaseCommand
+    {
+        private Action<T> ExecuteDelegate { get; }
+
+        public RelayCommand(Action<T> method, Func<bool> predicate = null) : base(predicate)
+        {
+            ExecuteDelegate = method ?? throw new ArgumentNullException(nameof(method));
+        }
+
+        public override void Execute(object parameter) => ExecuteDelegate.Invoke((T)parameter);
+    }
 }
